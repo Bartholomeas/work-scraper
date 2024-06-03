a522753c77f8bdafed6d48b6636eb83d70698953
Bartholomeas <69473527+Bartholomeas@users.noreply.github.com>
2024-06-03 22:44:06 +0200

    [Feat]: add shadcn and prettier and eslint (#11)
    Co-authored-by: Bartosz <barth.webdesign@gmail.com>

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import "@/style.css";
 import App from "./App.vue";
+import "./assets/index.css";
 
 createApp(App).mount("#app");
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import * as path from "node:path";
+import path from "path";
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [vue()],
   resolve: {
     alias: {
