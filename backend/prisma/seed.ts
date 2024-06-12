6e313256cb752c4d0ddc211f62584c34b32bae2e
Bartosz <barth.webdesign@gmail.com>
2024-06-12 21:46:57 +0200

    [Fix]: seeding

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -11,6 +11,15 @@ const workSchedules = ["full-time", "part-time", "internship", "freelance"];
 
 // const dataSources = ["pracuj", "justjoin"];
 
+function connectOrCreateField(values: string[]) {
+  return {
+    connectOrCreate: values.map(value => ({
+      where: { value },
+      create: { value },
+    })),
+  };
+}
+
 const mockOffer: JobOffer = {
   id: "aSDAJS4jasdj5r",
   slug: "",
@@ -41,52 +50,49 @@ async function main() {
         where: { positionName: mockOffer.positionName },
       })
       .catch(() => 0);
-    const positionLevelsExists = await prisma.positionLevel.findMany().catch(() => false);
-    const contractTypesExists = await prisma.contractType.findMany().catch(() => false);
-    const workModesExists = await prisma.workMode.findMany().catch(() => false);
-    const workSchedulesExists = await prisma.workSchedule.findMany().catch(() => false);
 
-    if (!positionLevelsExists || (Array.isArray(positionLevelsExists) && !positionLevelsExists.length))
-      await prisma.positionLevel.createMany({
-        data: positionLevels.map(p => ({ value: p })),
-      });
-
-    if (!contractTypesExists || (Array.isArray(contractTypesExists) && !contractTypesExists.length))
-      await prisma.contractType.createMany({
-        data: contractTypes.map(p => ({ value: p })),
-      });
-
-    if (!workModesExists || (Array.isArray(workModesExists) && !workModesExists.length))
-      await prisma.workMode.createMany({
-        data: workModes.map(p => ({ value: p })),
-      });
-
-    if (!workSchedulesExists || (Array.isArray(workSchedulesExists) && !workSchedulesExists.length))
-      await prisma.workSchedule.createMany({
-        data: workSchedules.map(p => ({ value: p })),
-      });
+    // const positionLevelsExists = await prisma.positionLevel.findMany().catch(() => false);
+    // const contractTypesExists = await prisma.contractType.findMany().catch(() => false);
+    // const workModesExists = await prisma.workMode.findMany().catch(() => false);
+    // const workSchedulesExists = await prisma.workSchedule.findMany().catch(() => false);
+    //
+    // if (!positionLevelsExists || (Array.isArray(positionLevelsExists) && !positionLevelsExists.length))
+    //   await prisma.positionLevel.createMany({
+    //     data: positionLevels.map(p => ({ value: p })),
+    //   });
+    //
+    // if (!contractTypesExists || (Array.isArray(contractTypesExists) && !contractTypesExists.length))
+    //   await prisma.contractType.createMany({
+    //     data: contractTypes.map(p => ({ value: p })),
+    //   });
+    //
+    // if (!workModesExists || (Array.isArray(workModesExists) && !workModesExists.length))
+    //   await prisma.workMode.createMany({
+    //     data: workModes.map(p => ({ value: p })),
+    //   });
+    //
+    // if (!workSchedulesExists || (Array.isArray(workSchedulesExists) && !workSchedulesExists.length))
+    //   await prisma.workSchedule.createMany({
+    //     data: workSchedules.map(p => ({ value: p })),
+    //   });
 
     await prisma.jobOffer.create({
       data: {
-        // slug: slugify(["Junior FS Dev", "Google", "intern", "costam"].join(" "), SLUGIFY_CONFIG),
         positionName: mockOffer.positionName!,
         slug: generateJobOfferSlug(mockOffer, existingOffersCount > 0 ? [(existingOffersCount + 1).toString()] : []),
-        // ...mockOffer,
-        positionLevels: {
-          connect: mockOffer?.positionLevels.map(value => ({ value })),
+        positionLevels: connectOrCreateField(mockOffer?.positionLevels),
+        contractTypes: connectOrCreateField(mockOffer?.contractTypes),
+        workModes: connectOrCreateField(mockOffer?.workModes),
+        workSchedules: connectOrCreateField(mockOffer?.workSchedules),
+        technologies: connectOrCreateField(mockOffer?.technologies),
+        workplaces: {
+          connectOrCreate: mockOffer?.workplaces?.map(value => ({
+            where: { value },
+            create: { value },
+          })),
         },
-        contractTypes: {
-          connect: mockOffer?.contractTypes.map(value => ({ value })),
-        },
-        workModes: {
-          connect: mockOffer?.workModes.map(value => ({ value })),
-        },
-        workSchedules: {
-          connect: mockOffer?.workSchedules.map(value => ({ value })),
-        },
-        technologies: {
-          connect: mockOffer?.technologies.map(value => ({ value })),
-        },
+        // workplaces: connectOrCreateField(mockOffer?.workplaces),
+
         company: {
           connectOrCreate: {
             where: { name: mockOffer.company?.name },
@@ -94,10 +100,6 @@ async function main() {
               name: mockOffer.company?.name ?? "Undefined name",
               logoUrl: null,
             },
-
-            // connect: {
-            //   name: mockOffer.company.name,
-            // },
           },
         },
       },
