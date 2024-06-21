e331975d652bde6cee70fc3030b78566457e11cb
Bartosz <barth.webdesign@gmail.com>
2024-06-22 00:29:02 +0200

    [Feat]: add categories handling

--- backend/src/components/offers/offers.service.ts
+++ backend/src/components/offers/offers.service.ts
@@ -40,11 +40,13 @@ class OffersService {
         ...OfferHelper.getDefaultParams(params),
         where: {
           ...OfferHelper.getSearchConditions(params?.search),
-          ...OfferHelper.getCategoriesConditions(params?.categories),
-          ...OfferHelper.getPositionLevelsConditions(params?.positionLevels),
-          ...OfferHelper.getContractTypesConditions(params?.contractTypes),
-          ...OfferHelper.getWorkModesConditions(params?.workModes),
-          ...OfferHelper.getWorkSchedulesConditions(params?.workSchedules),
+          AND: [
+            ...OfferHelper.getCategoriesConditions(params?.categories),
+            ...OfferHelper.getPositionLevelsConditions(params?.positionLevels),
+            ...OfferHelper.getContractTypesConditions(params?.contractTypes),
+            ...OfferHelper.getWorkModesConditions(params?.workModes),
+            ...OfferHelper.getWorkSchedulesConditions(params?.workSchedules),
+          ],
           // ...OfferHelper.getDataSourcesConditions(params?.dataSources),
         },
         include: {
@@ -215,86 +217,74 @@ class OfferHelper {
 
   public static getCategoriesConditions(categories?: OffersQueryParams["categories"]) {
     return Array.isArray(categories) && categories.length > 0
-      ? {
-          AND: categories.map(category => ({
-            technologies: {
-              some: {
-                value: category,
-              },
+      ? categories.map(category => ({
+          technologies: {
+            some: {
+              value: category,
             },
-          })),
-        }
-      : {};
+          },
+        }))
+      : [];
   }
 
   public static getPositionLevelsConditions(positionLevels?: OffersQueryParams["positionLevels"]) {
     return Array.isArray(positionLevels) && positionLevels.length > 0
-      ? {
-          AND: positionLevels.map(value => ({
-            positionLevels: {
-              some: {
-                value,
-              },
+      ? positionLevels.map(value => ({
+          positionLevels: {
+            some: {
+              value,
             },
-          })),
-        }
-      : {};
+          },
+        }))
+      : [];
   }
 
   public static getContractTypesConditions(contractTypes?: OffersQueryParams["contractTypes"]) {
     return Array.isArray(contractTypes) && contractTypes.length > 0
-      ? {
-          AND: contractTypes.map(value => ({
-            contractTypes: {
-              some: {
-                value,
-              },
+      ? contractTypes.map(value => ({
+          contractTypes: {
+            some: {
+              value,
             },
-          })),
-        }
-      : {};
+          },
+        }))
+      : [];
   }
 
   public static getWorkModesConditions(workModes?: OffersQueryParams["workModes"]) {
     return Array.isArray(workModes) && workModes.length > 0
-      ? {
-          AND: workModes.map(value => ({
-            workModes: {
-              some: {
-                value,
-              },
+      ? workModes.map(value => ({
+          workModes: {
+            some: {
+              value,
             },
-          })),
-        }
-      : {};
+          },
+        }))
+      : [];
   }
 
   public static getWorkSchedulesConditions(workSchedules?: OffersQueryParams["workSchedules"]) {
     return Array.isArray(workSchedules) && workSchedules.length > 0
-      ? {
-          AND: workSchedules.map(value => ({
-            workSchedules: {
-              some: {
-                value,
-              },
+      ? workSchedules.map(value => ({
+          workSchedules: {
+            some: {
+              value,
             },
-          })),
-        }
-      : {};
+          },
+        }))
+      : [];
   }
 
   public static getDataSourcesConditions(dataSources?: OffersQueryParams["dataSources"]) {
     return Array.isArray(dataSources) && dataSources.length > 0
-      ? {
-          AND: dataSources.map(value => ({
-            dataSource: {
-              some: {
-                value,
-              },
+      ? dataSources.map(value => ({
+          dataSource: {
+            some: {
+              value,
             },
-          })),
-        }
-      : {};
+          },
+        }))
+      : [];
   }
 
   public static parsePrismaToJobOffer<T extends PrismaJobOffer>(prismaOffers: T[]): JobOffer[] {
