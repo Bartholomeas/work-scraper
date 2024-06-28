3d7cce6f593f379ba235d0ecfb1df8e32f2464a1
Bartosz <barth.webdesign@gmail.com>
2024-06-29 00:06:06 +0200

    [Fix]: backend imports

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
 import tailwind from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
@@ -11,10 +10,6 @@ export default defineConfig({
       plugins: [tailwind(), autoprefixer()],
     },
   },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+
   plugins: [vue()],
 });
