dd7601c42880fa1aafe9f780160e6a9008f81545
Bartosz <barth.webdesign@gmail.com>
2024-06-30 11:15:31 +0200

    [Feat]: add offers layout

---
+++ apps/frontend/src/components/offers/filters/OffersPagination.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {
+  Pagination,
+  PaginationEllipsis,
+  PaginationFirst,
+  PaginationLast,
+  PaginationList,
+  PaginationListItem,
+  PaginationNext,
+  PaginationPrev,
+} from "@/components/ui/pagination";
+import { Button } from "@/components/ui/button";
+
+interface OffersPaginationProps {
+  totalPages: number;
+  currentPage: number;
+}
+
+const { totalPages, currentPage } = withDefaults(defineProps<OffersPaginationProps>(), {
+  totalPages: 0,
+  currentPage: 1,
+});
+</script>
+
+<template>
+  <Pagination>
+    <PaginationList>
+      <PaginationFirst />
+      <PaginationPrev />
+
+      <template v-for="(_, page) in Array.from({ length: totalPages })" :key="`paginationListItem-${page}`">
+        <PaginationListItem v-if="page" :value="page" as-child>
+          <Button>{{ page }}</Button>
+        </PaginationListItem>
+        <PaginationEllipsis v-else :key="`paginationListItem-${page}`" />
+      </template>
+
+      <PaginationNext />
+      <PaginationLast />
+    </PaginationList>
+  </Pagination>
+</template>
--- apps/frontend/src/views/Offers.vue
+++ apps/frontend/src/views/Offers.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
-import OffersFiltersContainer from "@/components/offers/filters/OffersFiltersContainer.vue";
+import OffersFiltersTopBar from "@/components/offers/filters/OffersFiltersTopBar.vue";
 import OffersItemsTable from "@/components/offers/OffersItemsList.vue";
 import OffersStatCards from "@/components/offers/OffersStatCards.vue";
+import OffersListLayout from "@/components/offers/OffersListLayout.vue";
+import OffersPagination from "@/components/offers/filters/OffersPagination.vue";
 </script>
 
 <template>
   <div class="flex flex-col gap-6 bg-accent-card">
     <h1 class="text-[40px] font-bold">Oferty pracy</h1>
     <OffersStatCards />
-    <OffersFiltersContainer />
-    <OffersItemsTable />
+    <OffersListLayout>
+      <template #top-bar>
+        <OffersFiltersTopBar />
+      </template>
+      <template #filters>
+        <div>FILTRY TU BEDA</div>
+      </template>
+      <OffersItemsTable />
+      <template #pagination>
+        <OffersPagination />
+      </template>
+    </OffersListLayout>
   </div>
 </template>
-
-<style scoped></style>
