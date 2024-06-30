17a637e450abb7b0ebaa70479a5ba327097f4b0d
Bartosz <barth.webdesign@gmail.com>
2024-06-30 23:51:14 +0200

    [Feat]: searchbar working

--- apps/frontend/src/api/offers/getOffers.ts
+++ apps/frontend/src/api/offers/getOffers.ts
@@ -3,12 +3,14 @@ import { useQuery } from "vue-query/esm";
 import type { JobOffersResponse, OffersQueryParams } from "shared/src/offers/offers.types";
 import { fetcher } from "@/utils/fetcher";
 import { type OffersQueryFunctionContext, offersQueryKeys } from "@/api/offers/offersQueryKeys";
+import { getSearchParamsAsString } from "@/utils/getSearchParamsAsString";
 
 // const getOffersList = async (key: OffersQueryFunctionContext["getOffers"]): Promise<JobOffersResponse | undefined> => {
-const getOffersList = async <T extends OffersQueryFunctionContext["getOffers"]>(key: T): Promise<JobOffersResponse | undefined> => {
-  console.log("kekew", key);
+const getOffersList = async ({ queryKey: [props] }: OffersQueryFunctionContext["getOffers"]): Promise<JobOffersResponse | undefined> => {
   try {
-    const url = `${import.meta.env.VITE_API_URL}/offers`;
+    const paramsUrl = getSearchParamsAsString(props?.params);
+
+    const url = `${import.meta.env.VITE_API_URL}/offers${paramsUrl}`;
     return await fetcher.get<JobOffersResponse>(url);
   } catch (err) {
     throw err;
