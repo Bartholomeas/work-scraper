c9e7322a21c948850880fa5ee54b6659c7d4cecb
Bartholomeas <69473527+Bartholomeas@users.noreply.github.com>
2024-07-16 14:33:56 +0200

    [Feat]: add guard-secret middleware (#38)
    Co-authored-by: Bartosz <barth.webdesign@gmail.com>

--- apps/backend/src/misc/constants.ts
+++ apps/backend/src/misc/constants.ts
@@ -12,6 +12,7 @@ export const BASE_URL = "/api/v1";
 //   // process.env.NODE_ENV === "test" ? "dupa" : process.env;
 // })();
 export const DATABASE_URL = process.env.DATABASE_URL ?? ("file:./dev.db?mode=memory&cache=shared" as string);
+export const { SECRET_PHRASE } = process.env;
 export const PORT = process.env.port;
 export const JWT_EXPIRES_IN = parseInt(process.env.JWT_EXPIRES_IN as string);
 export const JWT_SECRET = process.env.JWT_SECRET as string;
