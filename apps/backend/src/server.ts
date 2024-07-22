c9542862c5f4ec5cc5fcc2eeca105bb29c244733
Bartholomeas <69473527+Bartholomeas@users.noreply.github.com>
2024-07-22 14:26:13 +0200

    [Fix]: improve imports order in server.ts to fix env (#52)
    Co-authored-by: Bartosz <barth.webdesign@gmail.com>

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,23 +1,20 @@
-//URGENT: dotenv.config() MUST !!! be before other imports. Auto rearrange may change order and then app cannot get .env properties.
-// dotenv.config({
-//   path: `.env.${process.env.NODE_ENV}`,
-// });
-import dotenv from "dotenv";
 import "tsconfig-paths";
-import { app } from "@/app";
+import dotenv from "dotenv";
 
 const envFile = `.env.${process.env.NODE_ENV ?? "development"}`;
 dotenv.config({ path: envFile });
-console.log({ envFile, DBenv: process.env.DATABASE_URL });
 
-const port = process.env.PORT;
+(async () => {
+  const PORT = await import("@/misc/constants").then(res => res.PORT);
+  const app = await import("./app").then(res => res.app);
 
-const server = app.listen(port, () => {
-  console.log(`Server is running at port: ${port}`);
-});
-
-process.on("SIGTERM", () => {
-  server.close(() => {
-    console.log("💥 Process terminated!");
+  const server = app.listen(PORT, () => {
+    console.log(`Server is running at port: ${PORT}`);
   });
-});
+
+  process.on("SIGTERM", () => {
+    server.close(() => {
+      console.log("💥 Process terminated!");
+    });
+  });
+})();
