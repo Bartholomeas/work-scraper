b9110cfb8d7856de45f7cd7d6981201433f9b04d
Bartosz <barth.webdesign@gmail.com>
2024-08-23 20:21:58 +0200

    [Fx

--- apps/backend/src/components/offers/offers.controller.ts
+++ apps/backend/src/components/offers/offers.controller.ts
@@ -159,32 +159,6 @@ class OffersController {
       next(ErrorHandlerController.handleError(err));
     }
   };
-
-  //  Scrappers
-  public scrapePracujData = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      this.scrapperController?.scrapePracujData();
-      res.status(204).json({});
-    } catch (err) {
-      next(ErrorHandlerController.handleError(err));
-    }
-  };
-  public scrapeSolidJobsData = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      this.scrapperController?.scrapeSolidJobsData();
-      res.status(204).json({});
-    } catch (err) {
-      next(ErrorHandlerController.handleError(err));
-    }
-  };
-  public scrapeJustJoinData = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      this.scrapperController?.scrapeJustJoinData();
-      res.status(204).json({});
-    } catch (err) {
-      next(ErrorHandlerController.handleError(err));
-    }
-  };
 }
 
 export { OffersController };
