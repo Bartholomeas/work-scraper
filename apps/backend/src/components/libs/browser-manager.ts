ed84f0a6c4486c13a090874f9f962e020ea05fa6
Bartholomeas <69473527+Bartholomeas@users.noreply.github.com>
2024-08-24 10:38:21 +0200

    Revert "[Chore]: delete puppeteer config args as something is preventing scra…" (#99)
    This reverts commit d5c59782c56883817c6bf6a2d2007b52fa5d3181.

--- apps/backend/src/components/libs/browser-manager.ts
+++ apps/backend/src/components/libs/browser-manager.ts
@@ -21,26 +21,26 @@ class BrowserManager {
           headless: true,
           executablePath: executablePath(),
           args: [
-            // "--disable-gpu", // Disable GPU acceleration, especially useful on servers without a GPU.
-            // "--disable-dev-shm-usage", // Use disk files instead of shared memory, useful in environments like Docker with limited shared memory.
-            // "--disable-setuid-sandbox", // Disable setuid sandbox. Necessary on some servers, but could reduce security.
-            // "--aggressive-cache-discard", // Aggressively discard cache from memory to reduce RAM usage.
-            // "--disable-cache", // Completely disable browser cache to always fetch fresh data, at the cost of higher network usage.
-            // "--disable-application-cache", // Disable the application cache to reduce memory usage.
-            // "--disable-offline-load-stale-cache", // Prevent loading outdated data from cache in offline mode.
-            // "--disable-gpu-shader-disk-cache", // Disable caching GPU shaders on disk to save disk space.
-            // "--media-cache-size=0", // Set media cache size to zero to reduce memory usage for multimedia content.
-            // "--disk-cache-size=0", // Set disk cache size to zero to minimize disk space usage.
-            // "--disable-extensions", // Disable all browser extensions to reduce resource consumption.
-            // "--disable-default-apps", // Disable default Chromium apps to free up resources.
-            // "--mute-audio", // Mute audio since it's not needed for web scraping.
-            // "--no-default-browser-check", // Disable the check if Chromium is the default browser to avoid unnecessary prompts.
-            // "--disable-background-timer-throttling", // Disable throttling of background timers to improve performance of background scripts.
-            // "--disable-backgrounding-occluded-windows", // Prevent the browser from throttling windows that are not visible, keeping performance consistent.
-            // "--disable-notifications", // Disable notifications to reduce resource usage.
-            // "--disable-component-update", // Disable automatic component updates to reduce network usage.
-            // "--disable-sync", // Disable syncing with Google services to save resources.
-            // "--disable-images", // Disable images
+            "--disable-gpu", // Disable GPU acceleration, especially useful on servers without a GPU.
+            "--disable-dev-shm-usage", // Use disk files instead of shared memory, useful in environments like Docker with limited shared memory.
+            "--disable-setuid-sandbox", // Disable setuid sandbox. Necessary on some servers, but could reduce security.
+            "--aggressive-cache-discard", // Aggressively discard cache from memory to reduce RAM usage.
+            "--disable-cache", // Completely disable browser cache to always fetch fresh data, at the cost of higher network usage.
+            "--disable-application-cache", // Disable the application cache to reduce memory usage.
+            "--disable-offline-load-stale-cache", // Prevent loading outdated data from cache in offline mode.
+            "--disable-gpu-shader-disk-cache", // Disable caching GPU shaders on disk to save disk space.
+            "--media-cache-size=0", // Set media cache size to zero to reduce memory usage for multimedia content.
+            "--disk-cache-size=0", // Set disk cache size to zero to minimize disk space usage.
+            "--disable-extensions", // Disable all browser extensions to reduce resource consumption.
+            "--disable-default-apps", // Disable default Chromium apps to free up resources.
+            "--mute-audio", // Mute audio since it's not needed for web scraping.
+            "--no-default-browser-check", // Disable the check if Chromium is the default browser to avoid unnecessary prompts.
+            "--disable-background-timer-throttling", // Disable throttling of background timers to improve performance of background scripts.
+            "--disable-backgrounding-occluded-windows", // Prevent the browser from throttling windows that are not visible, keeping performance consistent.
+            "--disable-notifications", // Disable notifications to reduce resource usage.
+            "--disable-component-update", // Disable automatic component updates to reduce network usage.
+            "--disable-sync", // Disable syncing with Google services to save resources.
+            "--disable-images", // Disable images
           ],
         });
       return this.browser;
