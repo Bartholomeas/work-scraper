3da5dc0759a980cbd1994db7d3ea40b89fd39758
Bartholomeas <69473527+Bartholomeas@users.noreply.github.com>
2024-08-27 00:03:15 +0200

    [Fix]: minor content fixes (#110)
    Co-authored-by: Bartosz <barth.webdesign@gmail.com>

--- apps/frontend/src/components/layout/Footer.vue
+++ apps/frontend/src/components/layout/Footer.vue
@@ -11,10 +11,10 @@ const currentYear = computed(() => dayjs(Date.now()).get("year"));
 <template>
   <footer class="container py-8">
     <Card class="flex flex-col items-center gap-2 p-4 md:flex-row md:justify-between">
-      <Text align="center" color="muted" size="xs" as="span"
-        >A może strona pomogła znaleźć pracę?
-        <a href="https://buycoffee.to/bartholomeas" target="_blank" class="text-primary font-black">Dorzuć się na utrzymanie! ☕️</a></Text
-      >
+      <!--      <Text align="center" color="muted" size="xs" as="span"-->
+      <!--        >A może strona pomogła znaleźć pracę?-->
+      <!--        <a href="https://buycoffee.to/bartholomeas" target="_blank" class="text-primary font-black">Dorzuć się na utrzymanie! ☕️</a></Text-->
+      <!--      >-->
       <Text color="muted" size="xs">{{ currentYear }} &copy; Bartosz Stefaniak @ ITotyle</Text>
       <Text align="center" color="muted" size="xs"
         >Stworzone przez: <a href="https://bstefaniak.pl" class="text-primary font-bold" target="_blank">Bartosz Stefaniak</a></Text
