75354aed1301a71e34b8150b6360513b0ff11099
gbura <rage6999@wp.pl>
2024-08-27 13:53:38 +0200

    [FEAT]: Create news page

---
+++ apps/frontend/src/components/views/news/NewsCardsSkeleton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import NewsCardSkeleton from "./NewsCardSkeleton.vue";
+</script>
+
+<template>
+  <div class="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <NewsCardSkeleton :itemsCount="6" />
+  </div>
+</template>
--- apps/frontend/src/views/News.vue
+++ apps/frontend/src/views/News.vue
@@ -7,23 +7,45 @@ import { useGetNews } from "@/api/news/getNews";
 import Title from "@/components/common/title/Title.vue";
 
 import NewsCard from "@/components/views/news/NewsCard.vue";
+import NewsCardsSkeleton from "@/components/views/news/NewsCardSkeleton.vue";
 
 const title = useTitle();
 title.value = `Aktualności | ${PAGE_NAME}`;
 
-const { data: newsItems } = useGetNews();
+const { data: newsItems, isLoading } = useGetNews();
 </script>
 
 <template>
   <div class="bg-accent-card flex w-full flex-col gap-6">
     <Title order="h1" size="h1" class="font-bold">Aktualności</Title>
-    <Suspense>
-      <div class="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4">
+    <div v-if="isLoading">
+      <NewsCardsSkeleton />
+    </div>
+    <div class="grid gap-6 md:grid-cols-2" v-else>
+      <TransitionGroup name="card">
         <NewsCard v-for="newsItem in newsItems" :key="newsItem.id" :newsItem="newsItem" />
-      </div>
-      <template #fallback>
-        <p>test</p>
-      </template>
-    </Suspense>
+      </TransitionGroup>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.card-move,
+.card-enter-active {
+  transition:
+    transform 0.3s ease-out,
+    opacity 0.3s ease-out;
+}
+
+.card-leave-active {
+  transition:
+    transform 0.2s ease-out,
+    opacity 0.2s ease-out;
+}
+
+.card-enter-from,
+.card-leave-to {
+  opacity: 0;
+  transform: scale(1.05);
+}
+</style>
