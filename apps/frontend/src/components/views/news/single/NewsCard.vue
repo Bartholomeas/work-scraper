d3f064eeeaf55bf97b8b74d29218a2ef311af7fb
Bartosz <barth.webdesign@gmail.com>
2024-09-02 18:16:02 +0200

    [Chore]: minor improvements, add backend real news data

--- apps/frontend/src/components/views/news/single/NewsCard.vue
+++ apps/frontend/src/components/views/news/single/NewsCard.vue
@@ -17,11 +17,13 @@ const fullFormatDate = computed(() => {
 </script>
 
 <template>
-  <Card class="transform transition-transform duration-300 ease-out hover:scale-105">
-    <CardContent class="flex flex-col gap-4 py-6">
+  <Card
+    class="before:shadow-primary relative transform p-0 transition-transform duration-300 ease-out before:absolute before:inset-0 before:rounded-lg before:opacity-0 before:shadow-lg before:transition-opacity before:duration-300 before:ease-out hover:translate-y-[-2px] hover:before:opacity-50"
+  >
+    <CardContent class="flex flex-col gap-4 py-4">
       <Text as="p" size="xs" color="muted">{{ fullFormatDate }}</Text>
       <Text as="p" weight="bold" color="primary" size="xl">{{ newsItem.title }}</Text>
-      <Text as="p">{{ newsItem.shortDescription }}</Text>
+      <Text as="p" size="sm" color="muted">{{ newsItem.shortDescription }}</Text>
     </CardContent>
   </Card>
 </template>
